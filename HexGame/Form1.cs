7a83769c3c115f1405c9e1ec0302186a44717617
Andreas S. <vs2015acc@outlook.com>
2016-10-25 01:33:38 +0200

    123

--- HexGame/Form1.cs
+++ HexGame/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,21 +13,30 @@ namespace HexGame
 {
 	public partial class Form1 : Form
 	{
-		Board board;
 		public Form1()
 		{
-			
 			InitializeComponent();
-			board = new HexGame.Board(5);
+			this.SetStyle(
+			              ControlStyles.AllPaintingInWmPaint |
+			              ControlStyles.UserPaint |
+			              ControlStyles.DoubleBuffer,
+			              true);
+			typeof (Panel).InvokeMember("DoubleBuffered",
+			                            BindingFlags.SetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+			                            null,
+			                            panel1,
+			                            new object[] {true});
+			board = new HexGame.Board(7);
 		}
 
 		private void panel1_Paint(object sender, PaintEventArgs e)
 		{
 			e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
 
-			PaintBoard(e.Graphics, 80, 80, 25, board);
+			PaintBoard(e.Graphics, 100, 100, 25, board);
 		}
 
+		Board board;
 		Point[,][] hexagons;
 
 		void PaintBoard(Graphics g, int x, int y, int radius, Board board)
@@ -38,54 +48,46 @@ namespace HexGame
 
 			Pen borderpen = Pens.Black;
 			Brush color1 = Brushes.White;
+			Brush color2 = Brushes.Black;
+			float barwidth = 20;
 
 			hexagons = new Point[board.Size, board.Size][];
 
-			float x1 = x - (float)hr;
-			float y1 = y - radius - 30;
+			float x1 = x;
+			float y1 = y - radius - 20;
 
-			float x2 = x1 + 2 * board.Size * (float)hr;
+			float x2 = x1 + 2 * (board.Size - 1) * (float)hr;
 			float y2 = y1;
-			
-
-			g.FillEllipse(color1, x1 - 10, y1, 20, 20);
-			g.FillEllipse(color1, x2 - 10, y1, 20, 20);
-			g.DrawEllipse(borderpen, x1 - 10, y1, 20, 20);
-			g.DrawEllipse(borderpen, x2 - 10 , y1, 20, 20);
-			g.FillRectangle(Brushes.White, x1, y1, x2 - x1, 20);
-			g.DrawLine(borderpen, x1, y1, x2, y1 );
-			g.DrawLine(borderpen, x1, y1+ 20, x2, y1+ 20);
 
 
-			x1 = (board.Size+1) *(float)hr +(float)hr;
-			y1 = y  + board.Size * (float)(radius + hr2); 
+			PainBar(g, x1, y1, x2, y2, barwidth, borderpen, color1);
 
-			x2 = x1 + 2 * board.Size * (float)hr;
+			x1 = (float)Math.Round(x + (board.Size - 1) * (float)hr );
+			y1 = (float)Math.Round(y + (board.Size - 1) * (float)(radius + hr2) + radius+ 20) ; 
+
+			x2 = x1 + 2 * (board.Size-1) * (float)hr;
 			y2 = y1;
-			
-			g.FillEllipse(color1, x1 - 10, y1, 20, 20);
-			g.FillEllipse(color1, x2 - 10, y1, 20, 20);
-			g.DrawEllipse(borderpen, x1 - 10, y1, 20, 20);
-			g.DrawEllipse(borderpen, x2 - 10, y1, 20, 20);
-			g.FillRectangle(Brushes.White, x1, y1, x2 - x1, 20);
-			g.DrawLine(borderpen, x1, y1, x2, y1);
-			g.DrawLine(borderpen, x1, y1 + 20, x2, y1 + 20);
+
+			PainBar(g, x1, y1, x2, y2, barwidth, borderpen, color1);
+
+			var angle = 2 * Math.PI * 5 / 12;
+
+			x1 = x + (float)(Math.Cos(angle) * (radius + 20));
+			y1 = y + (float)(Math.Sin(angle) * (radius + 20));
+
+			x2 = x + (float)((( (board.Size - 1)) * hr) + Math.Cos(angle) * (radius + 20));
+			y2 = y + (float)((board.Size - 1) * (radius + hr2) + Math.Sin(angle) * (radius + 20));
+
+			PainBar(g, x1, y1, x2, y2, barwidth, borderpen, color2);
 
 
-			x1 = x - (float)hr - 40;
-			y1 = y - radius + (float) hr2;
+			x1 = x + (float)((2 * (board.Size - 1)  *hr) - Math.Cos(angle) * (radius + 20));
+			y1 = y - (float)(Math.Sin(angle) * (radius + 20));
 
-			y2 = y1 + (board.Size) * (float)(radius + hr2) -(float) hr2;
-			x2 = x1 +  (y2 - y1) *(float)((2*hr)/(radius+hr2)/2);
+			x2 = x + (float)(((3*(board.Size - 1)) * hr) - Math.Cos(angle) * (radius + 20));
+			y2 = y + (float)((board.Size - 1) * (radius + hr2) - Math.Sin(angle) * (radius + 20));
 
-
-			g.FillEllipse(color1, x1 - 10, y1 - 10, 20, 20);
-			g.FillEllipse(color1, x2 - 10, y2 - 10 , 20, 20);
-			g.DrawEllipse(borderpen, x1 - 10, y1 -10, 20, 20);
-			g.DrawEllipse(borderpen, x2 - 10, y2 - 10, 20, 20);
-			//g.FillRectangle(Brushes.White, x1, y1, x2 - x1, 20);
-			g.DrawLine(borderpen, x1, y1, x2, y2);
-			//g.DrawLine(borderpen, x1, y1 + 20, x2, y2 + 20);
+			PainBar(g, x1, y1, x2, y2, barwidth, borderpen, color2);
 
 			for (int ix = 0; ix < board.Size; ix++)
 			{
@@ -106,6 +108,35 @@ namespace HexGame
 				}
 			}
 
+		}
+
+		void PainBar(Graphics g, float x1, float y1, float x2, float y2, float width, Pen border, Brush color)
+		{
+
+			var dx = x2 - x1;
+			var dy = y2 - y1;
+			var angle = Math.Sin(dx / dy);
+			if (dy == 0)
+				angle = Math.Sign(dx) * 2 * Math.PI / 4;
+			var da = 2 * Math.PI / 4;
+			var offx = Math.Sin(angle + da) * width / 2;
+			var offy = Math.Cos(angle + da) * width / 2;
+
+			PointF[] poly = new PointF[5];
+			poly[0] = new PointF(x1 - (float)offx, y1 - (float)offy);
+			poly[1] = new PointF(x2 - (float)offx, y2 - (float)offy);
+			poly[2] = new PointF(x2 + (float)offx, y2 + (float)offy);
+			poly[3] = new PointF(x1 + (float)offx, y1 + (float)offy);
+
+			poly[4] = poly[0];
+
+			g.FillEllipse(color, x1 - width / 2, y1 - width / 2, width, width);
+			g.FillEllipse(color, x2 - width / 2, y2 - width / 2, width, width);
+			g.DrawEllipse(border, x1 - width / 2, y1 - width / 2, width, width);
+			g.DrawEllipse(border, x2 - width / 2, y2 - width / 2, width, width);
+			g.FillPolygon(color, poly);
+			g.DrawLine(border, poly[0], poly[1]);
+			g.DrawLine(border, poly[2], poly[3]);
 		}
 
 		Point[] PaintHexagon(Graphics g, int x, int y, double radius, double rotation = Math.PI / 6)
